a79d9c82c13d3dc202ca2b84bc7b9ccea0bfdb25
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2023-11-15 22:21:50 -0800

    Created rcs_core package

---
+++ rcs_core/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'rcs_core'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='damon',
+    maintainer_email='57426668+BruhSoundEffectNumber2@users.noreply.github.com',
+    description='Core systems for RCS.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
