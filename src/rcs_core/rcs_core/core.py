e8c1e8e2624b73295767d3c896bc91d25d41e429
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2023-12-07 16:57:43 -0800

    Update heartbeat, host docker

--- src/rcs_core/rcs_core/core.py
+++ src/rcs_core/rcs_core/core.py
@@ -5,69 +5,50 @@ from rclpy.clock import Time, Duration
 from rcs_interfaces.msg import Heartbeat
 
 
-ENTITY_NAME = "rcs_core"
-
-HEARTBEAT_TIMER_INTERVAL = 5.0
-"""About how long to wait between expected heartbeats."""
-
-HEARTBEAT_CHECK_INTERVAL = 1.0
-"""How often to check for missed heartbeats."""
-
-HEARTBEAT_GRACE_PERIOD = 3.0
-"""How long to wait after the expected heartbeat before declaring a failure."""
-
-COMPONENT_STATUS_INACTIVE = "inactive"
-COMPONENT_STATUS_ACTIVE = "active"
-COMPONENT_STATUS_FAILED = "failed"
+SELF_ENTITY_NAME = "rcs_core"
+GUI_ENTITY_NAME = "rover_gui"
 
 
 class Core(Node):
-    heartbeat_times: dict[str, Time] = {}
-    component_statuses: dict[str, str] = {"tester": "inactive"}
-    heartbeat_timer: Timer
-    heartbeat_sub: Subscription
-
     def __init__(self):
-        super().__init__(ENTITY_NAME)
-
-        self.heartbeat_timer = self.create_timer(
-            HEARTBEAT_CHECK_INTERVAL, self.heartbeat_timer_callback)
+        super().__init__(SELF_ENTITY_NAME)
 
         self.heartbeat_sub = self.create_subscription(
-            Heartbeat, f"{ENTITY_NAME}/heartbeat/sub", self.heartbeat_callback, 10)
+            Heartbeat,
+            f"/{GUI_ENTITY_NAME}/heartbeat/pub",
+            self.heartbeat_received,
+            10
+        )
+
+        self.heartbeat_pub = self.create_publisher(
+            Heartbeat,
+            f"/{GUI_ENTITY_NAME}/heartbeat/sub",
+            10
+        )
 
         self.get_logger().info("RCS Core initialized")
 
-    def heartbeat_timer_callback(self):
-        # Check for missed heartbeats
-        now = self.get_clock().now()
+    def heartbeat_received(self, msg: Heartbeat):
+        time_sent = Time(seconds=msg.header.stamp.sec,
+                         nanoseconds=msg.header.stamp.nanosec,
+                         clock_type=self.get_clock().clock_type)
+        time_received = self.get_clock().now()
 
-        for node_name, last_heartbeat in self.heartbeat_times.items():
-            # Check if the heartbeat is too old
-            if (now - last_heartbeat) > Duration(seconds=HEARTBEAT_TIMER_INTERVAL + HEARTBEAT_GRACE_PERIOD):
-                if self.component_statuses[node_name] != COMPONENT_STATUS_ACTIVE:
-                    continue
+        # Calculate trip time
+        trip_time = time_received - time_sent
 
-                # If it is, log the error
-                self.get_logger().error(f"Missed heartbeat from {node_name}!")
-                # Mark the component as failed
-                self.component_statuses[node_name] = COMPONENT_STATUS_FAILED
+        self.get_logger().info(
+            f"Heartbeat received from GUI after {str(trip_time)}")
 
-    def heartbeat_callback(self, msg: Heartbeat):
-        if msg.node_name not in self.component_statuses:
-            self.get_logger().warning(
-                f"Unexpected heartbeat from {msg.node_name}")
-            return
+        # Send a heartbeat back
+        self.send_hearbeat()
 
-        if self.component_statuses[msg.node_name] == COMPONENT_STATUS_FAILED:
-            self.get_logger().info(f"Reconnected to {msg.node_name}")
-        elif self.component_statuses[msg.node_name] == COMPONENT_STATUS_INACTIVE:
-            self.get_logger().info(f"Connected to {msg.node_name}")
+    def send_hearbeat(self):
+        msg = Heartbeat()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.entity_name = SELF_ENTITY_NAME
 
-        # Log the time we received the heartbeat
-        self.heartbeat_times[msg.node_name] = self.get_clock().now()
-        # Make sure we mark the component as active
-        self.component_statuses[msg.node_name] = COMPONENT_STATUS_ACTIVE
+        self.heartbeat_pub.publish(msg)
 
 
 def main(args=None):
